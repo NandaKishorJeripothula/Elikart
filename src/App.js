adee6e46415d9cfcd979ba2bee37b7aa59142e80
Willson <vermawillson@gmail.com>
2018-01-26 00:41:09 +0530

    redundent component removing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-//import SuperComponent1 from './components/SuperComponent1' ;
-//import SuperComponent2 from './components/SuperComponent2' ;
 import Toolbar2 from './components/Toolbar2' ;
 import AutoCom from './components/Autocom' ;
 import CategoryBar from './components/CategoryBar' ;
 import Slider from './components/Slider' ;
-import MyLog from './components/MyLog' ;
+//import MyLog from './components/MyLog' ;
 import LogIn from './components/LogIn' ;
 //import Carousel  from './components/Carousel' ;
-import Paper1 from './components/Paper1' ;
+//import Paper1 from './components/Paper1' ;
 
 
 
@@ -24,7 +22,7 @@ import Paper1 from './components/Paper1' ;
         
         
         <Slider/> 
-        <Paper1/>
+     
 
         </div>
         </MuiThemeProvider>
