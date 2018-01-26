a94801fc6aa78a5100b6e9bd6c97e0e6e595473c
NandaKishorJeripothula <j.kishor.bd@gmail.com>
2018-01-26 13:19:04 +0530

    basic router with emptyapp done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route,Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,16 +7,32 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+         <Router>
+         <div>
+           <ul>
+             <li><Link to="/">Home</Link></li>
+             <li><Link to="/about">About</Link></li>
+           </ul>
+     
+           <hr/>
+     
+           <Route exact path="/" component={Home}/>
+           <Route path="/about" component={About}/>
+         </div>
+       </Router>
+      </div>        
     );
   }
 }
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
 
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
 export default App;
