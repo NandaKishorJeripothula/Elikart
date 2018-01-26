33447800decd9c965554813e3c337a3fbf2aed2e
NandaKishorJeripothula <j.kishor.bd@gmail.com>
2018-01-26 18:47:47 +0530

    materail ui installed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route,Link,Switch } from 'react-router-dom';
+import Reboot from 'material-ui/Reboot';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+          <Reboot />
          <Router>
          <div>
            <ul>
