597fd41a46250861d9243b3d1450c592a03c7ffe
NandaKishorJeripothula <j.kishor.bd@gmail.com>
2018-01-31 21:56:59 +0530

    changes made to logo and responsiveness

--- src/components/Login_Signup.js
+++ src/components/Login_Signup.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GridList, { GridListTile, GridListTileBar } from 'material-ui/GridList';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
 import Divider from 'material-ui/Divider';
+import Hidden from 'material-ui/Hidden';
 import ELlikartLogo from './ElikartLogo.png';
 import './Login_Signup.css';
 export default class Login_Signup extends React.Component{
@@ -12,10 +12,12 @@ export default class Login_Signup extends React.Component{
 	render(){
 		return(
 			<div className="Login_Signup_body">
-			<MuiThemeProvider>
+				<Hidden mdDown>
 				<GridList cols={2} cellHeight={800}>
 					<GridListTile cols={1} className="loginGrid">
-									<img src={ELlikartLogo} className="responsive-img" alt="Elikart Logo "/>
+									<div className="center">
+										<img src={ELlikartLogo} className="responsive-img" alt="Elikart Logo "/>
+									</div>
 									<TextField id="Username"
 										required
 										className="textField"
@@ -81,8 +83,77 @@ export default class Login_Signup extends React.Component{
 						</GridListTile>
 					</GridListTile>
 				</GridList>
-			</MuiThemeProvider>
+				</Hidden>
+				<div className="mobileForm">
+					<Hidden mdUp>
+						<div className="center">
+							<img src={ELlikartLogo} className="responsive-img" alt="Elikart Logo "/>
+						</div>
+						<TextField id="Username"
+							required
+							className="textField"
+							placeholder="Username"
+						/>
+						<TextField id="Password"
+							placeholder="password"
+							type="password"
+							className="textField"
+							autoComplete="current-password"
+							required
+						/>
+						<br/>
+						<br/>
+						<Button raised color="secondary" >
+							Login
+						</Button>
+						<br/>
+						<h5>New to Elikart? SignUp below </h5>
+						<br/>
+						<TextField id="Firstname"
+							required
+							className="textField"
+							placeholder="Firstname"
+						/>
+						<TextField id="Lastname"
+							className="textField"
+							required
+							placeholder="Lastname"
+						/>
+						<TextField id="Uname"
+							className="textField"
+							required
+							placeholder="Username"
+						/>
+						<TextField id="email"
+							className="textField"
+							required
+							placeholder="email"
+						/>
+						<TextField id="Pword"
+							className="textField"
+							placeholder="password"
+							type="password"
+							required
+						/>
+						<TextField id="CPword"
+							className="textField"
+							placeholder=" confirm password"
+							type="password"
+							required
+						/>
+						<TextField id="mobile_no"
+							className="textField"
+							required
+							placeholder="Mobile Number"
+						/>
+						<br/>   
+						<br/>  
+						<Button raised color="secondary" >SignUp</Button>			
+					</Hidden>
+				</div>
+
 			</div>
+			
 			);
 	}
 }
