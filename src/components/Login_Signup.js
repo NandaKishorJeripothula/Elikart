48f9b3fee6c69ba609020889dad090b07af508ba
NandaKishorJeripothula <j.kishor.bd@gmail.com>
2018-01-31 19:25:16 +0530

    the loginpage styles fixed

--- src/components/Login_Signup.js
+++ src/components/Login_Signup.js
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom';
 import GridList, { GridListTile, GridListTileBar } from 'material-ui/GridList';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import TextField from 'material-ui/TextField';
-
 import Button from 'material-ui/Button';
 import Divider from 'material-ui/Divider';
-import FinalLogo2 from './FinalLogo2.png';
 import './Login_Signup.css';
 export default class Login_Signup extends React.Component{
 	
@@ -17,16 +15,17 @@ export default class Login_Signup extends React.Component{
 			<MuiThemeProvider>
 			<GridList cols={2} cellHeight={800}>
 			<GridListTile cols={1}>
-			<img id="logo" src={FinalLogo2}/>
 			<GridList cols={2}>
 			<GridListTile cols={1}>
 			 <TextField id="Username"
 			 required
+			 className="textField"
        		placeholder="Username"
        		/>
         	<TextField id="Password"
           placeholder="password"
-          type="password"
+		  type="password"
+		  className="textField"
           autoComplete="current-password"
         	required
         	/>
@@ -51,33 +50,47 @@ export default class Login_Signup extends React.Component{
 			
 			 <TextField id="Firstname"
 			 required
+			 className="textField"
+
        		placeholder="Firstname"
        		/><br/>
-       		 <TextField id="Lastname"
+				<TextField id="Lastname"
+							 className="textField"
+
 			 required
        		placeholder="Lastname"
        		/><br/>
-       		 <TextField id="Uname"
+				<TextField id="Uname"
+							 className="textField"
+
 			 required
        		placeholder="Username"
        		/><br/>
-       		 <TextField id="email"
+				<TextField id="email"
+							 className="textField"
+
 			 required
        		placeholder="email"
        		/><br/>
-       		 <TextField id="Pword"
+				<TextField id="Pword"
+							 className="textField"
+
          		placeholder="password"
          	 type="password"
           	
         	required
         	/><br/>
-        	<TextField id="CPword"
+			<TextField id="CPword"
+						 className="textField"
+
          		placeholder=" confirm password"
          	 type="password"
           
         	required
         	/><br/>
-        	<TextField id="mobile_no"
+			<TextField id="mobile_no"
+						 className="textField"
+
 			 required
        		placeholder="mobile no"
        		/><br/>
