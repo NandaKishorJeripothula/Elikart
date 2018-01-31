f5b1ee9700d749a8204405312210aba3acb4707c
NandaKishorJeripothula <j.kishor.bd@gmail.com>
2018-02-01 01:52:13 +0530

    all merges done

--- src/components/Login_Signup.js
+++ src/components/Login_Signup.js
@@ -5,10 +5,90 @@ import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
 import Divider from 'material-ui/Divider';
 import Hidden from 'material-ui/Hidden';
+import {saveOffline } from './config';
+import {authenticateUser, Userinfo, MakeUser} from './api';
 import ELlikartLogo from './ElikartLogo.png';
 import './Login_Signup.css';
 export default class Login_Signup extends React.Component{
+	constructor() {
+		super()
+		this.state = {
+		  username: '',
+		  password: '',
+			email:'',
+			phonenumber:'',
+		  firstname:'',
+			lastname:'',
+		};
+		}
+	  handleUsernameChange = (e) => {
+		this.setState({
+		  ...this.state,
+		  username: e.target.value
+		});
+	  }
+	  handlePasswordChange = (e) => {
+		this.setState({
+		  ...this.state,
+		  password: e.target.value
+		});
+	  }
+	  handleEmailChange = (e) => {
+		this.setState({
+		  ...this.state,
+		  email: e.target.value
+		});
+	  }
+	  handleFirstnameChange = (e) => {
+		this.setState({
+		  ...this.state,
+		  firstname: e.target.value
+		});
+		}
+		handlePhonenumChange = (e) => {
+			this.setState({
+				...this.state,
+				phonenumber: e.target.value
+			});
+			}
+	  handleLastnameChange = (e) => {
+		this.setState({
+		  ...this.state,
+		  lastname: e.target.value
+		});
+		} 
+
+		login = () => {
+			console.log('on login clicked');
+			authenticateUser(this.state.username, this.state.password, false).then(authResponse => {
+				console.log(authResponse);
+				if (authResponse.auth_token) {
+					//Save the auth token offline to be used by the filestore service
+					saveOffline(authResponse.auth_token)
+					Userinfo().then(Userinfo => {
+						console.log(JSON.stringify(Userinfo))
+					});
+				} else {
+					console.log(JSON.stringify(authResponse));
+				}
+			});
+		}
 	
+		register = () => {
+			console.log('on register clicked');
+			authenticateUser(this.state.username, this.state.password, true).then(authResponse => {
+				console.log(authResponse);
+				if (authResponse.auth_token) {
+					saveOffline(authResponse.auth_token)
+					MakeUser(authResponse.hasura_id,this.state.lastname,this.state.email,this.state.phonenumber,this.state.firstname).then(userres=>{
+						console.log(JSON.stringify(userres))
+					})
+					console.log("SignUp Successful! \n Your auth credentials are: " + JSON.stringify(authResponse, null, 2))
+				} else {
+					console.log(JSON.stringify(authResponse));
+				}
+			});
+		}
 	render(){
 		return(
 			<div className="Login_Signup_body">
@@ -19,11 +99,13 @@ export default class Login_Signup extends React.Component{
 										<img src={ELlikartLogo} className="responsive-img" alt="Elikart Logo "/>
 									</div>
 									<TextField id="Username"
+										onChange={this.handleUsernameChange}
 										required
 										className="textField"
 										placeholder="Username / Email"
 									/>
 									<TextField id="Password"
+										onChange={this.handlePasswordChange}
 										placeholder="password"
 										type="password"
 										className="textField"
@@ -32,7 +114,11 @@ export default class Login_Signup extends React.Component{
 									/>
 								<br/>
 								<br/>
-								<Button raised color="secondary" >
+								<Button 
+									raised 
+									color="secondary" 
+									onClick= {(e) => {this.login()}}
+									>
 									Login
 								</Button>
 					
@@ -41,45 +127,58 @@ export default class Login_Signup extends React.Component{
 						<h5>New to Elikart? SignUp below </h5>
 						<GridListTile>
 							<TextField id="Firstname"
+								onChange={this.handleFirstnameChange}
 								required
 								className="textField"
 								placeholder="Firstname"
 								/>
 							<TextField id="Lastname"
+								onChange={this.handleLastnameChange}
 								className="textField"
 								required
 								placeholder="Lastname"
 							/>
 							<TextField id="Uname"
+								onChange={this.handleUsernameChange}	
 								className="textField"
 								required
 								placeholder="Username"
 							/>
 							<TextField id="email"
+								onChange={this.handleEmailChange}
 								className="textField"
 								required
 								placeholder="email"
 							/>
-							<TextField id="Pword"
+							<TextField id="Password"
+								onChange={this.handlePasswordChange}
 								className="textField"
 								placeholder="password"
 								type="password"
 								required
 							/>
-							<TextField id="CPword"
+							<TextField id="ConfirmPassword"
+								onChange={this.handlePasswordChange}
 								className="textField"
 								placeholder=" confirm password"
 								type="password"
 								required
 							/>
 							<TextField id="mobile_no"
+								onChange={this.handlePhonenumChange}
 								className="textField"
 								required
 								placeholder="Mobile Number"
 							/>
 							<br/>   
 							<br/>  
-							<Button raised color="secondary" >SignUp</Button>
+							<Button 
+								onClick= {(e) => {this.register()}}
+								raised 
+								color="secondary" 
+								>
+								SignUp
+							</Button>
 						</GridListTile>
 					</GridListTile>
 				</GridList>
@@ -90,65 +189,84 @@ export default class Login_Signup extends React.Component{
 							<img src={ELlikartLogo} className="responsive-img" alt="Elikart Logo "/>
 						</div>
 						<TextField id="Username"
-							required
-							className="textField"
-							placeholder="Username / Email"
-						/>
-						<TextField id="Password"
-							placeholder="password"
-							type="password"
-							className="textField"
-							autoComplete="current-password"
-							required
-						/>
-						<br/>
-						<br/>
-						<Button raised color="secondary" >
-							Login
-						</Button>
+										onChange={this.handleUsernameChange}
+										required
+										className="textField"
+										placeholder="Username / Email"
+									/>
+									<TextField id="Password"
+										onChange={this.handlePasswordChange}
+										placeholder="password"
+										type="password"
+										className="textField"
+										autoComplete="current-password"
+										required
+									/>
+								<br/>
+								<br/>
+								<Button 
+									raised 
+									color="secondary" 
+									onClick= {(e) => {this.login()}}
+									>
+									Login
+								</Button>
 						<br/>
 						<h5>New to Elikart? SignUp below </h5>
 						<br/>
 						<TextField id="Firstname"
-							required
-							className="textField"
-							placeholder="Firstname"
-						/>
-						<TextField id="Lastname"
-							className="textField"
-							required
-							placeholder="Lastname"
-						/>
-						<TextField id="Uname"
-							className="textField"
-							required
-							placeholder="Username"
-						/>
-						<TextField id="email"
-							className="textField"
-							required
-							placeholder="email"
-						/>
-						<TextField id="Pword"
-							className="textField"
-							placeholder="password"
-							type="password"
-							required
-						/>
-						<TextField id="CPword"
-							className="textField"
-							placeholder=" confirm password"
-							type="password"
-							required
-						/>
-						<TextField id="mobile_no"
-							className="textField"
-							required
-							placeholder="Mobile Number"
-						/>
-						<br/>   
-						<br/>  
-						<Button raised color="secondary" >SignUp</Button>			
+								onChange={this.handleFirstnameChange}
+								required
+								className="textField"
+								placeholder="Firstname"
+								/>
+							<TextField id="Lastname"
+								onChange={this.handleLastnameChange}
+								className="textField"
+								required
+								placeholder="Lastname"
+							/>
+							<TextField id="Uname"
+								onChange={this.handleUsernameChange}	
+								className="textField"
+								required
+								placeholder="Username"
+							/>
+							<TextField id="email"
+								onChange={this.handleEmailChange}
+								className="textField"
+								required
+								placeholder="email"
+							/>
+							<TextField id="Password"
+								onChange={this.handlePasswordChange}
+								className="textField"
+								placeholder="password"
+								type="password"
+								required
+							/>
+							<TextField id="ConfirmPassword"
+								onChange={this.handlePasswordChange}
+								className="textField"
+								placeholder=" confirm password"
+								type="password"
+								required
+							/>
+							<TextField id="mobile_no"
+								onChange={this.handlePhonenumChange}
+								className="textField"
+								required
+								placeholder="Mobile Number"
+							/>
+							<br/>   
+							<br/>  
+							<Button 
+								onClick= {(e) => {this.register()}}
+								raised 
+								color="secondary" 
+								>
+								SignUp
+							</Button>			
 					</Hidden>
 				</div>
 
